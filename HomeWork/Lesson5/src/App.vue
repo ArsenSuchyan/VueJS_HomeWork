b71c447d0a31c495cd1c3deb35b338fa65c33862
ArsenSuchian <118843584+ArsenSuchyan@users.noreply.github.com>
2025-01-31 14:40:59 +0200

    Add files via upload

---
+++ HomeWork/Lesson5/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <product-cards-list></product-cards-list>
+    <laptop-shop />
+  </div>
+</template>
+
+<script>
+import ProductCardsList from "./components/ProductCardsList";
+import LaptopShop from "./components/LaptopShop.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductCardsList,
+    LaptopShop
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding-bottom: 50px;
+}
+</style>
---
+++ HomeWork/Lesson5/src/constants/constants.js
@@ -0,0 +1,55 @@
+export const berriesList = [
+  {
+    id: 1,
+    name: "FruitTime",
+    description: "Соковита та корисна чорниця, багата на антиоксиданти.",
+    inStock: true,
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQMY5uQKijywIHVwMZcIG0-hp01bIfJW7w0XUkADOXnpYeTUcdiJdegfzzPChbtbeQ6V4Mluc35MPK8ATRfhdEp5g",
+    shopUrl:
+      "https://fruit-time.ua/blog/chornicya-i-lohina-vidminni-risi-ta-korisni-vlastivosti.html"
+  },
+  {
+    id: 2,
+    name: "Сільпо",
+    description: "Свіжа полуниця з насиченим ароматом та солодким смаком.",
+    inStock: false,
+    imageUrl:
+      "https://images.silpo.ua/products/1600x1600/2ec02e2d-39ae-47b5-930b-a29cc8e93e6a.png",
+    shopUrl: "https://silpo.ua/product/polunytsia-51602"
+  },
+  {
+    id: 3,
+    name: "FruitMarket",
+    description: "Малина без хімії, вирощена в екологічно чистих умовах.",
+    inStock: true,
+    imageUrl:
+      "https://www.fruit-market.com.ua/wp-content/uploads/2020/04/malina.jpg",
+    shopUrl: "https://www.fruit-market.com.ua/product/malina"
+  }
+];
+
+export const laptops = [
+  {
+    id: 1,
+    image: "https://content.rozetka.com.ua/goods/images/big/507313400.jpg",
+    name: "ASUS ROG Strix G16",
+    price: 63000,
+    discountedPrice: 56700
+  },
+  {
+    id: 2,
+    image:
+      "https://estore.ua/ua/media/catalog/product/cache/9/image/600x600/9df78eab33525d08d6e5fb8d27136e95/m/a/macbook-air-m2-13_7_.jpeg",
+    name: "Apple MacBook Air M2",
+    price: 50400,
+    discountedPrice: 46200
+  },
+  {
+    id: 3,
+    image: "https://content.rozetka.com.ua/goods/images/big/494603108.jpg",
+    name: "Dell XPS 15",
+    price: 75600,
+    discountedPrice: 69300
+  }
+];
